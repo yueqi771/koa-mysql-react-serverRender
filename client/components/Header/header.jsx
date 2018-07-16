36c3c0aaee1d44cf0f6766c31dfaacefa4daba6c
MACHENI-GHD7VDB\越祈 <878895921@qq.com>
2018-07-16 21:34:53 +0800

    导航栏和首页 样式重构  ---- 2018/7/16

--- client/components/Header/header.jsx
+++ client/components/Header/header.jsx
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import "./header.less"
 
 class Header extends Component {
+    constructor(props, context) {
+        super(props, context)
+    }
+
     render() {
+        const { router } = this.context
         return (
-            <div className="header">
-                这里是导航栏
+            // style={{backgroundImage: 'url(http://localhost:3000/images/big.jpg)'}}
+            <div className="header-wrapper">
+                <div className="header">
+                    <img src="http://localhost:3000/images/logo.png" alt=""/>
+
+                    <div className="header-list">
+                        <p className="header-list-item" onClick={e => {router.history.push('/index')}}>首页</p>
+                        <p className="header-list-item" onClick={e => {router.history.push('/index')}}>技术杂谈</p>
+                        <p className="header-list-item" onClick={e => {router.history.push('/index')}}>关于自己</p>
+                        <p className="header-list-item" onClick={e => {router.history.push('/index')}}>时间轴</p>
+                        <p className="header-list-item" onClick={e => {router.history.push('/login')}}>登陆</p>
+                        <p className="header-list-item" onClick={e => {router.history.push('/register')}}>注册</p>
+                    </div>
+                </div>
             </div>
         )
     }
+}
+
+Header.contextTypes = {
+    router: PropTypes.object.isRequired
 }
 
 export default Header;
--- client/pages/Index/index.jsx
+++ client/pages/Index/index.jsx
@@ -10,17 +10,85 @@ import "./index.less"
     }
 }) @observer
 class Index extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            bannerText: ""
+        }
+    }
+
+    // async componentDidMount() {
+    //     await this.textAnimate('听见薄雾微光')
+    //         .then(res => {
+    //             console.log(23)
+    //         })
+    // }
+
+    // 文字逐渐出现效果
+    textAnimate = (text) => {
+        const { bannerText } = this.state;
+        let i = 0, 
+            showText = text;
+        let timer = setInterval(() => {
+            this.setState({
+                bannerText: text.substring(0, i)
+            }, () => {
+                i++;
+                if(bannerText == showText){
+                    clearInterval(timer);
+                };
+            })
+        }, 160);
+    }
+
+
     shouldComponentUpdate(nextProps, nextState) {
         if (_.isEqual(this.props, nextProps) && _.isEqual(this.state, nextState)) {
             return false
         }
         return true
     }
+
+
     render() {
+        const { bannerText } = this.state;
         return(
-            // {this.props.userInfo.name}
             <div className="index"> 
                 <Header />
+
+                {/* 主页banner */}
+                <div className="banner">
+                    <p className="banner-text">听见薄雾微光</p>
+                    <p className="banner-text2">---王东旭的个人博客</p>
+
+                </div> 
+                <div className="star-wrapper">
+                    <span className="star star01"></span>
+                    <span className="star star02"></span>
+                    <span className="star star03"></span>
+                    <span className="star star04"></span>
+                    <span className="star star05"></span>
+                    <span className="star star06"></span>
+                    <span className="star star07"></span>
+                    <span className="star star08"></span>
+                    <span className="star star09"></span>
+                    <span className="star star10"></span>
+                    <span className="star star11"></span>
+                    <span className="star star12"></span>
+                    <span className="star star13"></span>
+                    <span className="star star14"></span>
+                    <span className="star star15"></span>
+                    <span className="star star16"></span>
+                    <span className="star star17"></span>
+                    <span className="star star18"></span>
+                    <span className="star star19"></span>
+                    <span className="star star20"></span>
+                    <span className="star star21"></span>
+                    <span className="star star22"></span>
+                    <span className="star star23"></span>
+                    <span className="star star24"></span>
+                </div>
             </div>
         ) 
     }
