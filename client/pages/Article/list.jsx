d7ed562637ac6d0bd43322745cbcd6b107f3dd8e
MACHENI-GHD7VDB\越祈 <878895921@qq.com>
2018-08-20 22:34:41 +0800

    文章列表页面更新样式

--- client/pages/Article/list.jsx
+++ client/pages/Article/list.jsx
@@ -14,6 +14,15 @@ class ArticleList extends Component {
     }
 
     componentDidMount() {
+        document.getElementById('article_banner_img').style.width = document.documentElement.clientWidth;
+
+        // 监听屏幕尺寸变化， 调整banner大小
+        window.addEventListener('resize', () => {
+            let element = document.getElementById('article_banner_img');
+             element.style.width = document.documentElement.clientWidth;
+            console.log(element.offsetWidth)
+        })
+
         // 获取所有文章类型
         http.get({
             url: '/article/getType'
@@ -32,12 +41,13 @@ class ArticleList extends Component {
 
                 {/* banner图片 */}
                 <div className="article-banner">
-                    <img src="http://localhost:3000/images/bk4.jpg" alt=""/>
+                    <img id="article_banner_img" src="http://localhost:3000/images/bk4.jpg" alt=""/>
                 </div>
 
                 <div className="article-list-wrapper">
                     {/* 筛选文章类型 */}
                     <div className="article-filter">
+                        <p className="filter-classify">栏目: </p>
                         {
                             articleType.map((item, index) => (
                                 <p className="filter-item" key={item.id}>{item.type}</p> 
