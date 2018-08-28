23542e59bac2880324eb8196a01a690abc66c767
MACHENI-GHD7VDB\越祈 <878895921@qq.com>
2018-08-28 23:01:12 +0800

    no message

--- server/router/ssr.js
+++ server/router/ssr.js
@@ -2,8 +2,8 @@ const Router = require('koa-router');
 const serverRender = require('./server-render');
 const path = require('path')
 const fs = require('fs');
-const serverEntry = require('../../dist/server-entry');
-const template = fs.readFileSync(path.join(__dirname, "../../dist/server.ejs"), "utf-8");
+const serverEntry = require('../../static/dist/server-entry');
+const template = fs.readFileSync(path.join(__dirname, "../../static/dist/server.ejs"), "utf-8");
 
 const router = require('koa-router')();
 
